3d70e3aa1a2c4e55cb34477d18b4eb4ca0414152
IgnisCodex <90404350+IgnisCodex@users.noreply.github.com>
2024-11-02 19:55:45 +0000

    🏗️ Upload

---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import *
+from gpiozero import Servo
+from time import sleep
+
+app = Flask(__name__)
+
+# Define GPIO Pins for LEDs
+servo = [Servo(17)]
+
+# Render the HTML page with control interface
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/moveServo", methods=["POST"])
+def moveServo():
+    pos = request.form.get("pos")
+
+    if (pos == "left"):
+        servo.min()
+    elif (pos == "center"):
+        servo.mid()
+    elif (pos == "right"):
+        servo.max()
+
+    return "Servo: " + pos
+
+if (__name__ == "__main__"):
+    app.run(host="0.0.0.0", port=8080)
